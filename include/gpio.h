64635d8315bfd813ab63934a7c016a13c82d8b79
Mark Sungurov <mark.sungurov@gmail.com>
2020-08-26 23:11:41 +0300

    Add GPIO registers definitions: GPIOx_BSRR, GPIOx_BRR
    Signed-off-by: Mark Sungurov <mark.sungurov@gmail.com>

--- include/gpio.h
+++ include/gpio.h
@@ -36,6 +36,27 @@
 /* Pin mode (MODE[1:0]) values */
 #define GPIO_MODE_OUTPUT_2MHz		0x02
 
+/* --- GPIO Registers ------------------------------------------------------ */
+
+/* Port bit set/reset register (GPIOx_BSRR) */
+#define GPIO_BSRR(port)			MMIO32((port) + 0x10)
+#define GPIOA_BSRR			GPIO_BSRR(GPIOA)
+#define GPIOB_BSRR			GPIO_BSRR(GPIOB)
+#define GPIOC_BSRR			GPIO_BSRR(GPIOC)
+#define GPIOD_BSRR			GPIO_BSRR(GPIOD)
+#define GPIOE_BSRR			GPIO_BSRR(GPIOE)
+#define GPIOF_BSRR			GPIO_BSRR(GPIOF)
+#define GPIOG_BSRR			GPIO_BSRR(GPIOG)
+
+/* Port bit reset register (GPIOx_BRR) */
+#define GPIO_BRR(port)			MMIO16((port) + 0x14)
+#define GPIOA_BRR			GPIO_BRR(GPIOA)
+#define GPIOB_BRR			GPIO_BRR(GPIOB)
+#define GPIOC_BRR			GPIO_BRR(GPIOC)
+#define GPIOD_BRR			GPIO_BRR(GPIOD)
+#define GPIOE_BRR			GPIO_BRR(GPIOE)
+#define GPIOF_BRR			GPIO_BRR(GPIOF)
+#define GPIOG_BRR			GPIO_BRR(GPIOG)
 
 
 void gpio_set_mode(uint32_t gpioport, uint8_t mode, uint8_t cnf,
