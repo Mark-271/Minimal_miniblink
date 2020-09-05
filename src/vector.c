ef902b05be09e6a9a4f72453ef80f3d2e3e1927b
Mark Sungurov <mark.sungurov@gmail.com>
2020-09-05 22:00:53 +0300

    vector: Strip out even more
    Signed-off-by: Mark Sungurov <mark.sungurov@gmail.com>

--- src/vector.c
+++ src/vector.c
@@ -3,31 +3,10 @@
 
 int main(void);
 
-static void blocking_handler(void)
-{
-	while (1);
-}
-
-static void null_handler(void)
-{
-	/* Do nothing. */
-}
-
 __attribute__ ((section(".vectors")))
 struct vector_table vector_table = {
 	.initial_sp_value	= &_stack,
 	.reset			= reset_handler,
-	.nmi			= null_handler,
-	.hard_fault		= blocking_handler,
-
-	.memory_manage_fault	= blocking_handler,
-	.bus_fault		= blocking_handler,
-	.usage_fault		= blocking_handler,
-	.debug_monitor		= null_handler,
-
-	.sv_call		= null_handler,
-	.pend_sv		= null_handler,
-	.systick		= null_handler,
 };
 
 void __attribute__ ((weak)) reset_handler(void)
